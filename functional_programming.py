f1666a3b531205b2771bae8d04d814b08338c75d
lee-injae <ilee.injae@gmail.com>
2021-04-26 14:35:55 -0400

    functional_programming added

---
+++ functional_programming.py
@@ -0,0 +1,134 @@
+from functools import reduce
+
+my_list = [1,2,3]
+u_list = [10,20,30]
+t_list = (5,4,3)
+def multiply_by2(item): 
+    return item*2
+
+# print(multiply_by2([1,2,3]))
+
+
+# map, filter, zip, and reduce 
+
+# print(map(multiply_by2, [1,2,3]))
+# print(my_list)
+
+def only_odd(item): 
+    return item % 2 != 0 
+
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+# print(filter(only_odd, my_list))
+# print(my_list)
+
+# print(zip(my_list, u_list, t_list))
+
+# print(reduce(accumulator,my_list, 0))
+
+# ======================
+# exercise
+# ======================
+
+#1. Captialize all of the pet names and print the list
+my_pets = ['sisi', 'bibi', 'titi', 'marley']
+
+def caps(item):
+    return item.capitalize 
+
+# print(list(map(caps, my_pets)))
+
+#2. Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest. 
+my_strings = ['a', 'b', 'c', 'd', 'e']
+my_numbers = [5,4,3,2,1]
+
+# print(zip(my_strings, sorted(my_numbers)))
+
+#3. Filter the scores taht pass over 50%
+
+scores = [73,20,65,19,76,100,88]
+
+def over50(item):
+    return item > 50
+
+# print(filter(over50, scores))
+
+#4. Combine all of the numbers that are in a list of this file using reduce (my_numbers and scores). what is the total? 
+
+def add(acc, item):
+    return item + acc 
+
+# print (my_numbers + scores)
+
+# print(reduce(add, (my_numbers + scores)))
+
+# Lambda expressions 
+lambda param: action(parameter)
+
+# print(reduce(lambda acc, item: acc+item, my_list))
+
+m_list = [5,4,3]
+
+# print(map(lambda item: item **2, m_list))
+
+# sort
+a = [(0,2), (4,3), (9,9), (10,-1)]
+
+# print(a.sort(key=lambda x: x[1]))
+# print(a)
+
+#list, set, dictionary comprehension 
+
+m_listing = [char for char in 'hello']
+m_listing2 = [num for num in range(0,100)]
+m_listing3 = [num**2 for num in range(0,100)]
+m_listing4 = [num**2 for num in range(0,100) if num % 2 ==0]
+
+# print(m_listing4)
+
+m_listing = {char for char in 'hello'}
+m_listing2 = {num for num in range(0,100)}
+
+# print(m_listing)
+
+simple = {
+    'a': 1,
+    'b': 2
+}
+
+# print(simple.items())
+
+my_dict = {key: value**2 for key, value in simple.items() if value % 2 == 0}
+my_dict2 ={num: num*2 for num in [1,2,3]}
+
+# print(my_dict2)
+
+# Comprehension exercise 
+
+s_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+# duplicates = []
+# for value in s_list: 
+#     if s_list.count(value) > 1:
+#         if value not in duplicates:
+#             duplicates.append(value)
+
+# print(duplicates)
+duplicates = list(set([x for x in s_list if s_list.count(x) > 1]))
+
+print(duplicates)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- oop.py
+++ oop.py
@@ -71,47 +71,237 @@
 # print(player1.shout())
 
 #Given the below class:
-class Cat:
-    species = 'mammal'
-    cats = {}
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Cat:
+#     species = 'mammal'
+#     cats = {}
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
         
 
-    def set(self):
-    	cats[self.name] = self.age
+#     def set(self):
+#     	cats[self.name] = self.age
 
 # put cats in dict
 # compare age
 # return the oldest
     	
-def oldest(cat_dict):
- 	cats = {}
- 	cats[cat_dict.name] = cat_dict.age
- 	return cats
+# def oldest(cat_dict):
+#  	cats = {}
+#  	cats[cat_dict.name] = cat_dict.age
+#  	return cats
 
 
 # 1 Instantiate the Cat object with 3 cats
-cat1 = Cat("a", 1)
-cat2 = Cat('b', 2)
-cat3 = Cat('c', 3)
+# cat1 = Cat("a", 1)
+# cat2 = Cat('b', 2)
+# cat3 = Cat('c', 3)
 
 
 # 2 Create a function that finds the oldest cat
 #{}
-def get_oldest(*args):
-	return max(args)
-
-
+# def get_oldest(*args):
+# 	return max(args)
 
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
 
-print(f'The odest cat is {get_oldest(cat1.age, cat2.age, cat3.age)} years old')
+# print(f'The odest cat is {get_oldest(cat1.age, cat2.age, cat3.age)} years old')
+
+#users
+    
+# class User:
+    # def __init__(self, email):
+    #     self.email = email
+
+#     def sign_in(self):
+#         print('logged in')
+
+# class Wizard(User):
+#     def __init__(self, name, power):
+#         # super().__init__(email)
+#         self.name = name
+#         self.power = power
+
+#     def attack(self):
+#         # User.attack(self)
+#         print(f'attacking with power of {self.power}')
+           
+# class Archer(User):
+#     def __init__(self, name, arrows):
+#         self.name = name
+#         self.arrows = arrows
+
+#     def check_arrows(self):
+#         print(f'{self.arrows} remaining')
+    
+#     def run(self):
+#         print('ran fast')
+
+    # def attack(self):
+    #     print(f'attacking with arrows: arrows left- {self.num_arrows}')
+
+# class HybridBorg(Wizard, Archer):
+#     def __init__(self, name, power, arrows):
+#         Archer.__init__(self, name, arrows)
+#         Wizard.__init__(self, name, power)
+
+# borg1 = HybridBorg('oc', 10, 100)
+
+# # print(borg1.check_arrows())
+# print(borg1.attack())
+# print(borg1.sign_in())
+
+
+# # bb = User('b@b.com')
+# # print(bb.email)
+
+# wizard1 = Wizard('ocean', 50, 'a@a.com')
+# archer1 = Archer('injae', 100)
+
+# # print(dir(wizard1))
+
+# class Toy():
+#     def __init__(self, color, age):
+#         self.color = color
+#         self.age = age 
+#         self.my_dict = {
+#             'name': 'Yoyo',
+#             'has_pets': False
+#         }
+
+#     def __str__(self):
+#         return f'{self.color}'
+
+#     def __len__(self):
+#         return 5
+
+#     def __call__(self):
+#         print('called')
+
+#     def __getitem__(self, i):
+#         return self.my_dict[i]
+
+
+
+# action_figure = Toy('red', 0)
+# print(action_figure.__str__())
+# print(str(action_figure))
+# print(len(action_figure))
+# print(action_figure())
+# print(action_figure['name'])
 
 
 
 
+
+
+
+
+
+# print(wizard1)
+
+# print(wizard1)
+# print(wizard1.sign_in())
+# wizard1.attack()
+# archer1.attack()
+
+# print(isinstance(wizard1, object))
+
+# def p_attack(char):
+#     char.attack()
+
+# p_attack(archer1)
+
+# for char in [wizard1, archer1]:
+#     char.attack()
+
+# class Pets():
+#     animals = []
+#     def __init__(self, animals):
+#         self.animals = animals
+
+#     def walk(self):
+#         for animal in self.animals:
+#             print(animal.walk())
+
+# class Cat():
+#     is_lazy = True
+    
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age 
+
+#     def walk(self):
+#         return f'{self.name} is just walking around'
+
+# class Simon(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
+
+# class Sally(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
+
+class SuperList(list):
+    def __len__(self):
+        return 1000
+
+    
+superlist1 = SuperList()
+superlist1.append(4)
+# print(len(superlist1))
+
+# print(superlist1[0])
+# print(issubclass(list, object))
+
+
+#1 Add another Cat
+
+# cat1 = Cat('ocean', 2)
+# cat2 = Cat('ij', 3)
+# cat3 = Cat('karam', 4)
+
+# class Marley(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
+
+#2 Create a list of all of the pets (create 3 cat instances from the above)
+
+# my_cats = [Simon('simon', 3), Sally('sally', 4), Marley('marley',9)]
+
+# print(my_cats)
+#3 Instantiate the Pet class with all your cats use cariable my_pets
+
+# my_pets = Pets(my_cats)
+
+#4 Output all of the cats walking using the my_pets instance
+
+# my_pets.walk()
+
+
+#MRO - Method Resolution Order
+# class A:
+#     num = 10
+
+# class B(A):
+#     pass
+
+# class C(A):
+#     num = 1 
+
+# class D(B,C):
+#     pass 
+
+# print(D.mro())
+
+class X: pass
+class Y: pass
+class Z: pass
+class A(X,Y): pass
+class B(Y,Z): pass
+class M(B,A,Z): pass
+
+print(M.__mro__)
 
 
 
