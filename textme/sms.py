25388c803c89457c8633b87ca08d0b0f92113381
lee-injae <github email address>
2021-06-03 13:04:42 -0400

    sms.py added

---
+++ textme/sms.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client 
+ 
+account_sid = 'AC42fe612fbaeff1a5f1e152e73af4738c' 
+auth_token = '[AuthToken]' 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create(  
+                              messaging_service_sid='MG01151a06da6d7aabc4c4afd8e1ca1f56',       
+                              body='this is so COOL',
+                              to='+1999999999' 
+                          ) 
+ 
+print(message.sid)
