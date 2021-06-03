540ad18ef9dc2acb110ede42f737f7671ffd50c3
Injae Lee <github email address>
2021-06-03 01:21:22 -0400

    ileebot tweeter bot added

--- ileebot/tweetweet.py
+++ ileebot/tweetweet.py
@@ -1,4 +1,5 @@
 import tweepy
+from tweepy.api import API
 
 auth = tweepy.OAuthHandler('qloLkyjCnI1WfuVpzuHL6IKte', 'Ml1kmM6f1SRltwgvogFy9Q8uFEEtagsuDIgDXyvQWEGvjw1Xla')
 auth.set_access_token('78420609-6N9Nit4Liyk1GinFHTRVu7l2kFYkzll3yq408inB0', 'eYkrmZxZgswcapzOrQcOJ6Ya0DNUn8zkm1rTKQQFRNdDT')
@@ -20,10 +21,25 @@ def limit_handler(cursor):
     except tweepy.RateLimitError:
         time.sleep(500)
 
+# Narcisst Bot 
 
-#Generous Bot 
-for follower in limit_handler(tweepy.Cursor(api.followers).items()):
-   if follower.followers_count > 1000:
-       follower.follow()
-       break
+search_string = '이인재'
+numberOfTweets = 5
+
+for tweet in tweepy.Cursor(api.search, search_string).items(numberOfTweets):
+    try: 
+        tweet.favorite()
+        print('I liked that tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break 
+
+
+
+# Generous Bot 
+# for follower in limit_handler(tweepy.Cursor(api.followers).items()):
+#    if follower.followers_count > 1000:
+#        follower.follow()
+#        break
 
