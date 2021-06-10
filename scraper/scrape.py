7649aca1dbe29506c7c21d028874be8480064ac4
Injae Lee <ilee.injae@gmail.com>
2021-06-10 04:13:57 -0400

    scraper.py updated

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -3,13 +3,24 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+res = requests.get('https://news.ycombinator.com/news?p=2')
 
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res.text, 'html.parser')
 
 links = soup.select('.storylink')
 # votes = soup.select('.score')
 subtext = soup.select('.subtext')
-ds
+
+links2 = soup2.select('.storylink')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2 
+mega_subtext = subtext + subtext2 
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
+
 def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
@@ -21,7 +32,7 @@ def create_custom_hn(links, subtext):
             if points > 99:
             # print(points)
                 hn.append({'title': title, 'link': href, 'votes': points})
-    return hn 
+    return sort_stories_by_votes(hn)
 
-pprint.pprint(create_custom_hn(links, subtext))
-
+# pprint.pprint(create_custom_hn(mega_links, mega_subtext))
+pprint.pprint(soup)
