404cee5f2806c303649b8d5a153bd99257ee6b52
Injae Lee <ilee.injae@gmail.com>
2021-06-17 04:57:11 -0400

    database practice with txt and csv

--- web-server/venv/server.py
+++ web-server/venv/server.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, url_for
+from os import EX_TEMPFAIL
+from flask import Flask, render_template, url_for, request, redirect
+from flask.wrappers import Request
+import csv 
+
 app = Flask(__name__)
 print((__name__))
 
@@ -10,9 +14,29 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
-# @app.route('/components.html')
-# def components():
-#     return render_template('components.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else: 
+        return 'something is wrong'
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database: 
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email}, {subject}, {message}')
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2: 
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
 
 
